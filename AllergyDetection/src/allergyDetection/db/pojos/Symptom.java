bb74d4d0a740ef29884ee15d2640b3d56c8d4513
Pablohitaa <pablo.hitalara@usp.ceu.es>
2024-04-21 12:18:50 +0200

    Symptoms and treatments done

--- AllergyDetection/src/allergyDetection/db/pojos/Symptom.java
+++ AllergyDetection/src/allergyDetection/db/pojos/Symptom.java
@@ -2,5 +2,30 @@ package allergyDetection.db.pojos;
 
 public class Symptom {
 	
+	private Integer id;
+	private String symptom_type;
+	
+	public Symptom (Integer _id, String _symptom_type) {
+		this.id=_id;
+		this.symptom_type=_symptom_type;
+	}
+	
+	 public Integer getId() {
+	        return id;
+	    }
 
+	 public void setId(Integer _id) {
+	        this.id = _id;
+	    }
+	 public String getSymptomType() {
+		 return symptom_type;
+	 }
+	 public void setSymptomType(String _symptom_type) {
+		 this.symptom_type= _symptom_type;
+	 }
+	 
+	 public String toString() {
+	    	return "Symptom [id=" + id + ", symptom type=" + symptom_type + "]";
+	    }
+	    
 }
--- AllergyDetection/src/allergyDetection/db/pojos/Treatment.java
+++ AllergyDetection/src/allergyDetection/db/pojos/Treatment.java
@@ -1,5 +1,32 @@
 package allergyDetection.db.pojos;
 
 public class Treatment {
+	
+	private Integer id;
+	private String treatment_type;
+	
+	public Treatment (Integer _id, String _treatment_type) {
+		this.id=_id;
+		this.treatment_type=_treatment_type;
+	}
+	
+	 public Integer getId() {
+	        return id;
+	    }
+
+	 public void setId(Integer _id) {
+	        this.id = _id;
+	    }
+	 public String getTreatmentType() {
+		 return treatment_type;
+	 }
+	 public void setTreatmentType(String _treatment_type) {
+		 this.treatment_type= _treatment_type;
+	 }
+	 
+	 public String toString() {
+	    	return "Treatment [id=" + id + ", treatment type=" + treatment_type + "]";
+	    }
+	    
 
 }
