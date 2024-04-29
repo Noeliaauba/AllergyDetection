9b5ef3e08f8c735cfbc91d7c98f559005d69807c
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-04-29 17:41:02 +0200

    Interfaces created

--- AllergyDetection/src/allergyDetection/db/interfaces/TreatmentManager.java
+++ AllergyDetection/src/allergyDetection/db/interfaces/TreatmentManager.java
@@ -1,7 +1,7 @@
 package allergyDetection.db.interfaces;
-
+import allergyDetection.db.pojos.Treatment;
 public interface TreatmentManager {
-	
-	public void addTreatment(); 
-
+	public void addTreatment(Treatment t); 
+	public void modifyTreatment(Integer id); //Del paciente
+	public void deleteTreatment(Integer id);
 }
