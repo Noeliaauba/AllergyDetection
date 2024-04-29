f8295e6be5646d82c7b57706df675c7d168388e1
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-04-29 18:08:16 +0200

    .

--- AllergyDetection/src/allergyDetection/db/interfaces/TreatmentManager.java
+++ AllergyDetection/src/allergyDetection/db/interfaces/TreatmentManager.java
@@ -4,4 +4,5 @@ public interface TreatmentManager {
 	public void addTreatment(Treatment t); 
 	public void modifyTreatment(Integer id); //Del paciente
 	public void deleteTreatment(Integer id);
+	//Search Treatmentfrompatientid
 }
