5b512c786c541b6eab2884d9cf322c5c717d67d6
carmeencaballero <carmen.caballeroherreros@usp.ceu.es>
2024-04-30 16:58:52 +0200

    ConnectionManager done

---
+++ AllergyDetection/src/allergyDetection/db/jdbc/JCDBCSymptomManager.java
@@ -0,0 +1,30 @@
+package allergyDetection.db.jdbc;
+
+import allergyDetection.db.interfaces.SymptomManager;
+import allergyDetection.db.pojos.Symptom;
+
+public class JCDBCSymptomManager implements SymptomManager {
+
+	public JCDBCSymptomManager(ConnectionManager connectionManager) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addSymptom(Symptom s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modifySymptom(Symptom s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteSymptom(Symptom s) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- AllergyDetection/src/allergyDetection/db/jdbc/JDBCDoctorManager.java
+++ AllergyDetection/src/allergyDetection/db/jdbc/JDBCDoctorManager.java
@@ -1,5 +1,18 @@
 package allergyDetection.db.jdbc;
 
+import allergyDetection.db.interfaces.DoctorManager;
+import allergyDetection.db.pojos.Doctor;
+
 public class JDBCDoctorManager implements DoctorManager {
+
+	public JDBCDoctorManager(ConnectionManager connectionManager) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addDoctor(Doctor d) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
