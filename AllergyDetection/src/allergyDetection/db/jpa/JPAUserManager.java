7392ff7b7a77b53640dd6b381bf9dc1b07cecc28
Pablohitaa <pablo.hitalara@usp.ceu.es>
2024-05-13 17:15:45 +0200

    x

--- AllergyDetection/src/allergyDetection/db/jpa/JPAUserManager.java
+++ AllergyDetection/src/allergyDetection/db/jpa/JPAUserManager.java
@@ -1,5 +1,28 @@
 package allergyDetection.db.jpa;
 
-public class JPAUserManager {
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+
+import library.db.pojos.Role;
+
+public class JPAUserManager implements UserManager {
+	
+}
+	private EntityManager em;
+	
+	public JPAUserManager() {
+		em = Persistence.createEntityManagerFactory("library-provider").createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		// Create default roles
+		// If they don't exist already
+		try {
+			this.getRole("librarian");
+		} catch(NoResultException e) {
+			this.createRole(new Role("librarian"));
+			this.createRole(new Role("borrower"));
+		}
 
 }
--- AllergyDetection/src/allergyDetection/db/pojos/Role.java
+++ AllergyDetection/src/allergyDetection/db/pojos/Role.java
@@ -1,5 +1,16 @@
 package allergyDetection.db.pojos;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class Role {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "roles")
+
+public class Role implements Serializable {
+	
+	
+			
 }
