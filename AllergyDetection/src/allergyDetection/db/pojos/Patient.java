4e6718b37788313bf1202bd73dd6a82242c2e100
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-05-31 23:25:58 +0200

    Fixing errors with Carmen :)

--- AllergyDetection/src/allergyDetection/db/pojos/Patient.java
+++ AllergyDetection/src/allergyDetection/db/pojos/Patient.java
@@ -20,7 +20,7 @@ import allergyDetection.db.xml.utils.SQLDateAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "Patient")
-@XmlType(propOrder = { "name", "surname", "gender" })
+@XmlType(propOrder = { "name", "surname", "dob","gender" })
 public class Patient implements Serializable {
 
 		private static final long serialVersionUID = 5415639098561047229L;
@@ -30,7 +30,7 @@ public class Patient implements Serializable {
 		private String name;
 		@XmlElement
 		private String surname;
-		@XmlTransient
+		@XmlElement
 		@XmlJavaTypeAdapter(SQLDateAdapter.class)
 		private Date dob;
 		@XmlTransient
